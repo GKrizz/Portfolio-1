9aa6a5a2d3a633395982cf83868aa1b828d8a0ac
Gowri K <gowrikalimuthu2003@gmail.com>
2023-08-07 14:10:55 +0530

    About me

---
+++ main.js
@@ -0,0 +1,10 @@
+var typed =new Typed(".text",{
+    strings: ['Fontend Developer.', 'UI/UX Designer.','Web Developer.'],
+    typeSpeed: 100,
+    backSpeed:  100,
+    backDelay: 1000,
+    loop: true
+
+
+
+ });
