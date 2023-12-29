5d4315bff1cd7a1b340334370ff5f8915ed3ba7f
Gowri K <gowrikalimuthu2003@gmail.com>
2023-12-30 01:26:05 +0530

    recent works

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-var typed =new Typed(".text",{
-    strings: ['Fontend Developer.', 'UI/UX Designer.','Web Developer.'],
-    typeSpeed: 100,
-    backSpeed:  100,
-    backDelay: 1000,
-    loop: true
+// var typed =new Typed(".text",{
+//     strings: ['Fontend Developer.', 'UI/UX Designer.','Web Developer.'],
+//     typeSpeed: 100,
+//     backSpeed:  100,
+//     backDelay: 1000,
+//     loop: true
 
 
 
- });
- const hamburger = document.querySelector(".hamburger");
- const navMenu = document.querySelector(".nav-menu");
+//  });
+//  const hamburger = document.querySelector(".hamburger");
+//  const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click",()=>{
-hamburger.classList.toggle("active");
-navMenu.classList.toggle("active");
-})
- document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click" ,() =>{
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+// hamburger.addEventListener("click",()=>{
+// hamburger.classList.toggle("active");
+// navMenu.classList.toggle("active");
+// })
+//  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click" ,() =>{
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
 
- }))
+//  }))
