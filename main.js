ae636ee5d37a5bc6ad2571a7fa8698150df88946
Gowri K <gowrikalimuthu2003@gmail.com>
2023-12-30 18:33:19 +0530

    media query

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-// var typed =new Typed(".text",{
-//     strings: ['Fontend Developer.', 'UI/UX Designer.','Web Developer.'],
-//     typeSpeed: 100,
-//     backSpeed:  100,
-//     backDelay: 1000,
-//     loop: true
+var typed =new Typed(".text",{
+    strings: ['Fontend Developer.', 'UI/UX Designer.','Web Developer.'],
+    typeSpeed: 100,
+    backSpeed:  100,
+    backDelay: 1000,
+    loop: true
 
 
 
-//  });
+ });
 //  const hamburger = document.querySelector(".hamburger");
 //  const navMenu = document.querySelector(".nav-menu");
 
@@ -20,3 +20,42 @@
 //     navMenu.classList.remove("active");
 
 //  }))
+window.addEventListener('load',()=>{
+    let allTab = document.querySelectorAll('.tab');
+    let allproject = document.querySelectorAll('.project');
+
+    const removeActive=() => allTab.forEach(tab =>{
+        tab.classList.remove('tab-active')
+    })
+    allTab.forEach(tab=>{
+        tab.addEventListener('click',(event) => {
+            removeActive();
+            tab.classList.add('tab-active');
+            let fliterName = event.target.getAttribute('data-name');
+            allproject.forEach(project=>{
+                let projectName = project.getAttribute('data-name');
+                if((projectName == fliterName) || fliterName == 'All'){
+                    project.style.display = 'block';
+                } else{
+                        project.style.display = 'none';
+                }
+    
+            })
+        });
+       
+    })
+})
+
+
+
+window.addEventListener('scroll',() =>{
+    let nav= document.querySelector('nav');
+    nav.classList.toggle('nav-scroll', window.scrollY>0);
+})
+
+//nav bar
+let menuBar = document.querySelector('.menu-bar');
+let menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click',() =>{
+    menuBar.classList.toggle('menu-none')
+})
