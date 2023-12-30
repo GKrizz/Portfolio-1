9ab80928a77d19ef9769a3a3fbd305b8392865f6
Gowri K <gowrikalimuthu2003@gmail.com>
2023-12-30 20:27:20 +0530

    js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed =new Typed(".text",{
-    strings: ['Fontend Developer.', 'UI/UX Designer.','Web Developer.'],
+    strings: ['Frontend Developer.', 'UI/UX Designer.','Web Developer.'],
     typeSpeed: 100,
     backSpeed:  100,
     backDelay: 1000,
