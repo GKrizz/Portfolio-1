614305e1f081d1410810e1a6fa74ea4b27db6764
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2024-02-26 23:54:48 +0530

    commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed =new Typed(".text",{
-    strings: ['Frontend Developer.', 'UI/UX Designer.','Web Developer.'],
+    strings: ['FullStack Developer.', 'Java Developer.','Python Developer.'],
     typeSpeed: 100,
     backSpeed:  100,
     backDelay: 1000,
